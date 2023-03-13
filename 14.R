fd52fdb7b38ea77517a5571b18d6bae3e06e3ee3
XinningSun <127236207+XinningSun@users.noreply.github.com>
2023-03-13 18:16:03 +0000

    Update 14.R

--- 14.R
+++ 14.R
@@ -12,23 +12,19 @@ library(ggpubr)
 
 #Import data into R
 coffee <- read.csv("https://raw.githubusercontent.com/martacasero7/DAS-Group-14/main/dataset14.csv")
+
 #Clean the data by removing missing values.
 coffee <- na.omit(coffee)
-#
-coffee$Quality_class <- ifelse(coffee$Qualityclass == "Good", "yes", "no")
 
+#Select the data
 coffee.select <- coffee %>%
-  select(aroma, flavor, acidity, category_two_defects, altitude_mean_meters, harvested, Quality_class)
+  select(aroma, flavor, acidity, category_two_defects, altitude_mean_meters, Qualityclass)
+coffee.select$Qualityclass <- as.factor(coffee.select$Qualityclass)
 
-coffee.data <- coffee.select %>%
-  mutate_if(is.character, as.factor)
-
-coffee.data$harvested <- factor(coffee.data$harvested)
-
-levels(coffee.data$Quality_class) <- c("Poor", "Good")
-
-rownames(coffee.data) <- NULL
 #GLM
-model <- glm(Quality_class ~ aroma + flavor + acidity + category_two_defects + altitude_mean_meters + harvested, 
-             data = coffee.data,
+model <- glm(Qualityclass ~ aroma + flavor + acidity +category_two_defects + altitude_mean_meters,
+             data = coffee.select,
              family = binomial(link = "logit"))
+summ(model)
+plot_model(model, show.values = TRUE,
+           title = "", show.p = FALSE, value.offset = 0.25)
