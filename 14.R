5311e2c5b655d4b80b9fbcddf8b535235cf57b99
XinningSun <127236207+XinningSun@users.noreply.github.com>
2023-03-13 18:32:33 +0000

    Update 14.R

--- 14.R
+++ 14.R
@@ -9,6 +9,8 @@ library(stats)
 library(jtools)
 library(reshape2)    
 library(ggpubr)
+library(GGally)
+library(janitor)
 
 #Import data into R
 coffee <- read.csv("https://raw.githubusercontent.com/martacasero7/DAS-Group-14/main/dataset14.csv")
@@ -17,14 +19,51 @@ coffee <- read.csv("https://raw.githubusercontent.com/martacasero7/DAS-Group-14/
 coffee <- na.omit(coffee)
 
 #Select the data
-coffee.select <- coffee %>%
+coffee <- coffee %>%
   select(aroma, flavor, acidity, category_two_defects, altitude_mean_meters, Qualityclass)
-coffee.select$Qualityclass <- as.factor(coffee.select$Qualityclass)
+coffee$Qualityclass <- as.factor(coffee$Qualityclass)
+levels(coffee$Qualityclass) <- c("Poor","Good")
+
+#Visualize the data
+
+ggplot(data = coffee, aes(x = Qualityclass, y = aroma, fill = Qualityclass)) +
+  geom_boxplot() +
+  labs(x = "Qualityclass", y = "Aroma") +
+  theme(legend.position = "none")
+
+ggplot(data = coffee, aes(x = Qualityclass, y = flavor, fill = Qualityclass)) +
+  geom_boxplot() +
+  labs(x = "Qualityclass", y = "Flavor") +
+  theme(legend.position = "none")
+
+ggplot(data = coffee, aes(x = Qualityclass, y = acidity, fill = Qualityclass)) +
+  geom_boxplot() +
+  labs(x = "Qualityclass", y = "Acidity") +
+  theme(legend.position = "none")
+
+ggplot(data = coffee, aes(x = Qualityclass, y = category_two_defects, fill = Qualityclass)) +
+  geom_boxplot() +
+  labs(x = "Qualityclass", y = "Category_two_defects") +
+  theme(legend.position = "none")
+
+ggplot(data = coffee, aes(x = Qualityclass, y = altitude_mean_meters, fill = Qualityclass)) +
+  geom_boxplot() +
+  labs(x = "Qualityclass", y = "Altitude_mean_meters") +
+  theme(legend.position = "none")
+
+#
+coffee %>%
+  tabyl(aroma, Qualityclass) %>%
+  adorn_percentages() %>%
+  adorn_pct_formatting() %>%
+  adorn_ns()
 
 #GLM
 model <- glm(Qualityclass ~ aroma + flavor + acidity +category_two_defects + altitude_mean_meters,
              data = coffee.select,
              family = binomial(link = "logit"))
+
 summ(model)
+
 plot_model(model, show.values = TRUE,
            title = "", show.p = FALSE, value.offset = 0.25)
