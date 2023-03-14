8a2c052b803b6d419f810905d2f6964a023d6b05
XinningSun <127236207+XinningSun@users.noreply.github.com>
2023-03-14 21:32:11 +0000

    Update 14.R

--- 14.R
+++ 14.R
@@ -19,8 +19,28 @@ coffee <- na.omit(coffee)
 #Select the data
 coffee <- coffee %>%
   select(aroma, flavor, acidity, category_two_defects, altitude_mean_meters, Qualityclass)
+
+coffee$Qualityclass <- ifelse(coffee$Qualityclass == "Poor", 0, 1)
+
+# Define a function to remove outliers.
+##This code defines a function called "remove_outliers", which takes two arguments: a data frame (df) and a variable name (var_name). The function calculates the lower and upper bounds for the variable using the first and third quartiles (quantiles) and the interquartile range (IQR). It then subsets the data frame by removing any rows where the variable value is outside of the lower and upper bounds. Finally, the function returns the modified data frame.)
+remove_outliers <- function(df, var_name) {
+  quantiles <- quantile(df[[var_name]], probs=c(0.25,0.75), na.rm=FALSE)
+  IQR_val <- IQR(df[[var_name]])
+  Lower_val <- quantiles[1]-1.5*IQR_val
+  Upper_val <- quantiles[2]+1.5*IQR_val
+  df <- subset(df, df[[var_name]] > Lower_val & df[[var_name]] < Upper_val)
+  return(df)
+}
+
+## Call the above function for each variable that needs to remove outliers.
+coffee <- remove_outliers(coffee, "aroma")
+coffee <- remove_outliers(coffee, "flavor")
+coffee <- remove_outliers(coffee, "acidity")
+coffee <- remove_outliers(coffee, "category_two_defects")
+coffee <- remove_outliers(coffee, "altitude_mean_meters")
+
 coffee$Qualityclass <- as.factor(coffee$Qualityclass)
-levels(coffee$Qualityclass) <- c("Good","Poor")
 
 #Visualize the data
 
@@ -49,7 +69,7 @@ ggplot(data = coffee, aes(x = Qualityclass, y = altitude_mean_meters, fill = Qua
   labs(x = "Qualityclass", y = "Altitude_mean_meters") +
   theme(legend.position = "none")
 
-#
+#Show original counts
 coffee %>%
   tabyl(aroma, Qualityclass) %>%
   adorn_percentages() %>%
@@ -81,21 +101,23 @@ coffee %>%
   adorn_ns()
 
 #GLM
-model <- glm(Qualityclass ~ aroma + flavor + acidity +category_two_defects + altitude_mean_meters,
+##Fit the GLM model
+model1 <- glm(Qualityclass ~ aroma + flavor + acidity + category_two_defects + altitude_mean_meters,
+             data = coffee,
+             family = binomial(link = "logit"))
+summ(model1)
+
+##Remove the values of category_two_defects and fit the GLM model
+model2 <- glm(Qualityclass ~ aroma + flavor + acidity + altitude_mean_meters,
              data = coffee,
              family = binomial(link = "logit"))
 
-summ(model)
+summ(model2)
 
-plot_model(model, show.values = TRUE,
-           title = "", show.p = FALSE, value.offset = 0.25)
-
-model %>%
+model2 %>%
   coef() %>%
   exp()
 
-plot_model(model,show.values = TRUE,
-           title = "Odds(Good/Poor)",show.p = FALSE)
-
+#Add the fitted values to the coffee data frame.
 coffee <- coffee %>%
-  mutate(Qualityclass_fitted = fitted(model))
+  mutate(Qualityclass_fitted = fitted(model2))
